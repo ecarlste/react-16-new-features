a408704581f777f37979560f55a7d03c4af60475
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-26 20:27:21 -0600

    Messing around with multiple state objects

--- src/index.js
+++ src/index.js
@@ -4,19 +4,24 @@ import * as serviceWorker from "./serviceWorker";
 
 const App = props => {
   const [count, setCount] = useState(props.count);
+  const [text, setText] = useState(props.text);
 
   return (
     <div>
-      <p>The current count is {count}</p>
+      <p>
+        The current {text} is {count}
+      </p>
       <button onClick={() => setCount(count - 1)}>-1</button>
       <button onClick={() => setCount(count + 1)}>+1</button>
       <button onClick={() => setCount(props.count)}>reset</button>
+      <input value={text} onChange={e => setText(e.target.value)} />
     </div>
   );
 };
 
 App.defaultProps = {
-  count: 10
+  count: 10,
+  text: "count"
 };
 
 ReactDOM.render(<App count={15} />, document.getElementById("root"));
