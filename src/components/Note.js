7c6697fcbc1551a5e1ba9282bf43a7e5ef88d65e
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-26 23:52:57 -0600

    Playing around with a custom hook for mouse pos

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,26 +1,23 @@
-import React, { useContext, useEffect } from "react";
-import NotesContext from "../context/notes-context";
+import React, { useContext } from "react";
 import actions from "../actions/actionTypes";
+import NotesContext from "../context/notes-context";
+import useMousePosition from "../hooks/useMousePosition.hook";
 
 const Note = ({ note }) => {
   const { dispatch } = useContext(NotesContext);
+  const position = useMousePosition();
 
   const removeNote = title => {
     dispatch({ type: actions.removeNoteAction, title });
   };
 
-  useEffect(() => {
-    console.log("Note component mounted");
-
-    return () => {
-      console.log("Note component unmounted");
-    };
-  }, []);
-
   return (
     <>
       <h3>{note.title}</h3>
       <p>{note.body}</p>
+      <p>
+        {position.x}, {position.y}
+      </p>
       <button onClick={() => removeNote(note.title)}>x</button>
     </>
   );
