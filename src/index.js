0c54198804c0179a677cceef3540b307a449b254
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-26 20:19:11 -0600

    Playing with state enabled functional components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<div>my new content...</div>, document.getElementById("root"));
+const App = props => {
+  const [count, setCount] = useState(props.count);
+
+  return (
+    <div>
+      <p>The current count is {count}</p>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(props.count)}>reset</button>
+    </div>
+  );
+};
+
+App.defaultProps = {
+  count: 10
+};
+
+ReactDOM.render(<App count={15} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
