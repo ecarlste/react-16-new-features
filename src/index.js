ad9fa39d6f972884f7f7862703645eb58e6aebaf
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-26 21:02:08 -0600

    Playing with forms using state hooks

--- src/index.js
+++ src/index.js
@@ -2,29 +2,45 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
-const App = props => {
-  const [count, setCount] = useState(props.count);
-  const [text, setText] = useState(props.text);
+const NotesApp = () => {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const onSubmit = event => {
+    event.preventDefault();
+    setNotes([...notes, { title, body }]);
+    setTitle("");
+    setBody("");
+  };
+
+  const onRemove = title => {
+    setNotes(notes.filter(note => note.title !== title));
+  };
 
   return (
     <div>
-      <p>
-        The current {text} is {count}
-      </p>
-      <button onClick={() => setCount(count - 1)}>-1</button>
-      <button onClick={() => setCount(count + 1)}>+1</button>
-      <button onClick={() => setCount(props.count)}>reset</button>
-      <input value={text} onChange={e => setText(e.target.value)} />
+      <h3>Notes</h3>
+      <form onSubmit={onSubmit}>
+        <input value={title} onChange={e => setTitle(e.target.value)} />
+        <textarea value={body} onChange={e => setBody(e.target.value)} />
+        <button>Add Note</button>
+      </form>
+      <div>
+        {notes.map(note => (
+          <div key={note.title}>
+            {note.title}
+            {note.body}
+            <button onClick={() => onRemove(note.title)}>x</button>
+            <br />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
-App.defaultProps = {
-  count: 10,
-  text: "count"
-};
-
-ReactDOM.render(<App count={15} />, document.getElementById("root"));
+ReactDOM.render(<NotesApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
